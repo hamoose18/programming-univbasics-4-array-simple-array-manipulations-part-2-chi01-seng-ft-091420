8c07ca0f2f3f452beb83ecceb0b3071bb6ebcf55
arhamm ghazali <arhamm_ghazali93@hotmail.com>
2020-09-08 18:55:42 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,17 @@
+def using_concat(array1, array2)
+  array1 + array2
+end
+
+def using_insert(array, element)
+  array.insert(4, element)
+end
+
+def using_uniq(array)
+  array.using
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def
