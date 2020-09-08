81507d9e4060b2672422c499f25f6b903461269c
arhamm ghazali <arhamm_ghazali93@hotmail.com>
2020-09-08 18:57:56 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -14,6 +14,10 @@ def using_flatten(array)
   array.flatten
 end
 
-def using_delete(array)
-  
+def using_delete(array, element)
+  array.delete(element)
+end
+
+def using_delete_at(array, index)
+  array.delete_at(index)
 end
